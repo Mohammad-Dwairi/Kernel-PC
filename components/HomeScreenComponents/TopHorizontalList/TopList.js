2b967ddcdd977e78018845f5a1ba57177287bed1
Mohammad_Dwairi <mdwairy@gmail.com>
2021-01-13 04:24:39 +0200

    Add special products auto-scrolling

--- components/HomeScreenComponents/TopHorizontalList/TopList.js
+++ components/HomeScreenComponents/TopHorizontalList/TopList.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import { View, FlatList } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, FlatList, TouchableWithoutFeedback } from 'react-native';
 import Product from '../../SharedComponents/Product/Product';
 import AppText from '../../SharedComponents/Atomic/AppText';
 import { topListStyle } from '../Styles';
 import { useSelector } from 'react-redux';
+import AppButton from '../../SharedComponents/Atomic/AppButton';
 
 
+const ItemSeprator = () => (
+    <View style={{ width: 20 }} />
+);
 // Contains special products on the top of HomeScreen.
 const TopList = props => {
+    let index = 0;
+    let interval;
+    const list = useRef();
 
     const darkMode = useSelector(state => state.darkMode.isDark);
     const styles = topListStyle(darkMode);
 
     const renderSpecialProduct = product => <Product product={product.item} />;
 
+    const scrollToIndex = () => {
+        try {
+            list.current.scrollToIndex({ animated: true, index: index++ });
+            if (index > 5) {
+                index = 0;
+            }
+        }
+        catch (err) {
+
+        }
+
+    };
+
+    interval = setInterval(() => {
+        scrollToIndex();
+    }, 2500);
+
+
     return (
         <View style={styles.container}>
             <AppText style={styles.text}>{props.title}</AppText>
             <FlatList
-                horizontal
+                ref={list}
+                horizontal={true}
+                onTouchStart={() => clearInterval(interval)}
+                //ItemSeparatorComponent={ItemSeprator}
                 pagingEnabled={true}
                 showsHorizontalScrollIndicator={false}
                 removeClippedSubviews={true}
--- components/SharedComponents/Product/Product.js
+++ components/SharedComponents/Product/Product.js
@@ -1,31 +1,22 @@
 import React from 'react';
-import { View, TouchableOpacity, ImageBackground, Platform, ActivityIndicator } from 'react-native';
+import { View, TouchableOpacity, ImageBackground, Platform } from 'react-native';
 import AppText from '../Atomic/AppText';
 import { useNavigation } from '@react-navigation/native';
 import AddToCartButton from './ProductButtons/AddToCartButton';
 import AddToWishlistButton from './ProductButtons/AddToWishListButton';
 import { productStyle } from './Styles';
 import { COLORS } from '../../../constants/colors/colors';
-import { useDispatch } from 'react-redux';
 
 const Product = props => {
 
     const navigation = useNavigation();
     const styles = productStyle();
-    const dispatch = useDispatch();
-
-    const onPressHandler = () => {
-        dispatch({type: 'SET_PRODUCT', product: product});
-        navigation.navigate('ProductScreen', { screen: 'Details', params: { product: props.product }} );
-    };
-
 
     return (
         <TouchableOpacity
             style={{ ...styles.container, ...props.style }}
             activeOpacity={Platform.OS === 'ios' ? 0.7 : 0.8}
             onPress={() => navigation.navigate('ProductScreen', { screen: 'Details', params: { product: props.product } })}
-            //onPress={onPressHandler}
             >
             <ImageBackground resizeMode='contain' style={styles.image} source={{ uri: props.product.images[0] }}>
 
@@ -36,7 +27,6 @@ const Product = props => {
                 <View style={styles.titleContainer}>
                     <AppText style={styles.text}>{props.product.brand + " " + props.product.model}</AppText>
                     <View style={{ flexDirection: 'row' }}>
-                        {/* <Entypo name='price-tag' color={COLORS.primary} size={25} /> */}
                         <AppText style={{ color: COLORS.text, fontFamily: 'open-sans-m' }}>JOD {props.product.price.toFixed(2)}</AppText>
                     </View>
                 </View>
